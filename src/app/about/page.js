f2f9225649f606ad794b51b92dd3e0ee9d3db1bd
jdavyds <slimjdavids@gmail.com>
2023-08-12 11:38:14 +0100

    update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,23 @@
 import Hero from "@/components/about/Hero";
+import Statements from "@/components/about/Statements";
 import Welcome from "@/components/about/Welcome";
 import Where from "@/components/about/Where";
 import Why from "@/components/about/Why";
-import Join from '@/components/home/Join'
+import Join from "@/components/home/Join";
+import bg from "@/assets/abt-bg.svg";
 export default function Home() {
   return (
     <main className="flex flex-col gap-10 justify-center z-[0] overflow-x-hidden relative ">
       <Hero />
-      <Welcome />
-      <Why />
-      <Where />
+      <div
+        className="bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${bg.src})` }}
+      >
+        <Welcome />
+        <Why />
+        <Where />
+      </div>
+      <Statements />
       <Join />
     </main>
   );
